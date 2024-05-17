bbb3396f33dbbbb0fbb06a5df1640c6e2fad92a3
Lida-Tellez <lidae.tellezg@gmail.com>
2024-05-17 09:31:04 -0500

    conflicto local

--- src/PartsUnlimited.WebJobs.ProcessOrder/Program.cs
+++ src/PartsUnlimited.WebJobs.ProcessOrder/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+//prueba conflicto local
 using System;
 using System.Linq;
 using Microsoft.Azure.WebJobs;
